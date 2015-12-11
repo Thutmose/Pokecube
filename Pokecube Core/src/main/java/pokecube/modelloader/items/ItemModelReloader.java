a7eff17ff70008a5b2c6a9d08884477b6958b848
Thutmose <elpatricimo@gmail.com>
2015-12-10 20:41:55 -0400

    Added dratini line to the resource pack
    models for dratini line were provided by sunwize_light who contacted me
    from curse.com

--- Pokecube Core/src/main/java/pokecube/modelloader/items/ItemModelReloader.java
+++ Pokecube Core/src/main/java/pokecube/modelloader/items/ItemModelReloader.java
@@ -5,7 +5,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import pokecube.core.database.Database;
-import pokecube.core.database.PokedexEntry;
+import pokecube.modelloader.client.custom.animation.AnimationLoader;
 
 public class ItemModelReloader extends Item
 {
@@ -21,16 +21,8 @@ public class ItemModelReloader extends Item
     public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer player)
     {
         if (!world.isRemote) return itemstack;
-        
-        System.out.println();
-        
-        for(PokedexEntry e: Database.allFormes)
-        {
-            if(e.getPokedexNb() == 201)
-                System.out.println(e);
-        }
-        
-        
+        Database.updateSizes();
+        AnimationLoader.load();
 //        if (!player.isSneaking()) AnimationLoader.load();
 //        else
 //        {

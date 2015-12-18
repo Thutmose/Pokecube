33ede9eb975bbc8ba05a97f3f3b3b1c51b9198f0
Thutmose <elpatricimo@gmail.com>
2015-12-17 23:59:35 -0400

    some more documentation

--- Pokecube Core/src/main/java/pokecube/core/events/ClassGenEvent.java
+++ Pokecube Core/src/main/java/pokecube/core/events/ClassGenEvent.java
@@ -5,15 +5,23 @@ import org.objectweb.asm.tree.ClassNode;
 
 import net.minecraftforge.fml.common.eventhandler.Event;
 
+/** This is event is sent for each generic pokemob class generated.<br>
+ * It is mostly sent during the pre-init phase, when the main classes are being
+ * made.<br>
+ * It is sent right after the node has accepted the classwriter.
+ * 
+ * @author Thutmose */
 public class ClassGenEvent extends Event
 {
-	public final ClassWriter writer;
-	public final ClassNode node;
-	public final int pokedexNb;
-	public ClassGenEvent(ClassWriter cw, ClassNode n, int nb) {
-		this.writer = cw;
-		pokedexNb = nb;
-		this.node = n;
-	}
+    public final ClassWriter writer;
+    public final ClassNode   node;
+    public final int         pokedexNb;
+
+    public ClassGenEvent(ClassWriter cw, ClassNode n, int nb)
+    {
+        this.writer = cw;
+        pokedexNb = nb;
+        this.node = n;
+    }
 
 }

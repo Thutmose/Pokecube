7a55dbe6e24b1d813e7c701b94c54febad705560
Thutmose <elpatricimo@gmail.com>
2015-12-31 16:09:26 -0400

    fixes nbtedit gui showing up infront of curser.

--- Pokecube Core/src/main/java/com/mcf/davidee/nbteditpqb/forge/ClientProxy.java
+++ Pokecube Core/src/main/java/com/mcf/davidee/nbteditpqb/forge/ClientProxy.java
@@ -22,106 +22,140 @@ import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 import pokecube.core.client.render.PTezzelator;
 
-public class ClientProxy extends CommonProxy {
+public class ClientProxy extends CommonProxy
+{
 
-	@Override
-	public void registerInformation(){
-		MinecraftForge.EVENT_BUS.register(this);
-		SaveStates save = NBTEdit.getSaveStates();
-		save.load();
-		save.save();
-	}
+    @Override
+    public void registerInformation()
+    {
+        MinecraftForge.EVENT_BUS.register(this);
+        SaveStates save = NBTEdit.getSaveStates();
+        save.load();
+        save.save();
+    }
 
-	@Override
-	public File getMinecraftDirectory(){
-		return FMLClientHandler.instance().getClient().mcDataDir;
-	}
+    @Override
+    public File getMinecraftDirectory()
+    {
+        return FMLClientHandler.instance().getClient().mcDataDir;
+    }
 
-	@Override
-	public void openEditGUI(int entityID, NBTTagCompound tag) {
-		Minecraft.getMinecraft().displayGuiScreen(new GuiEditNBTTree(entityID, tag));
-	}
-	
-	@Override
-	public void openEditGUI(BlockPos pos, NBTTagCompound tag) {
-		Minecraft.getMinecraft().displayGuiScreen(new GuiEditNBTTree(pos, tag));
-	}
+    @Override
+    public void openEditGUI(int entityID, NBTTagCompound tag)
+    {
+        new GuiOpener(entityID, tag);
+    }
 
-	@SubscribeEvent
-	public void renderWorldLast(RenderWorldLastEvent event){
-		GuiScreen curScreen = Minecraft.getMinecraft().currentScreen;
-		if (curScreen instanceof GuiEditNBTTree){
-			GuiEditNBTTree screen = (GuiEditNBTTree)curScreen;
-			Entity e = screen.getEntity();
-			
-			if (e != null && e.isEntityAlive())
-				drawBoundingBox(event.context, event.partialTicks,e.getEntityBoundingBox());
-			else if (screen.isTileEntity()){
-				int x = screen.getBlockX();
-				int y = screen.y;
-				int z = screen.z;
-				World world = Minecraft.getMinecraft().theWorld;
-				BlockPos pos = new BlockPos(x, y, z);
-				Block b = world.getBlockState(pos).getBlock();
-				if (b != null) {
-					b.setBlockBoundsBasedOnState(world, pos);
-					drawBoundingBox(event.context, event.partialTicks, b.getSelectedBoundingBox(world, pos));
-				}
-			}
-		}
-	}
+    @Override
+    public void openEditGUI(BlockPos pos, NBTTagCompound tag)
+    {
+        new GuiOpener(pos, tag);
+    }
 
-	private void drawBoundingBox(RenderGlobal r, float f, AxisAlignedBB aabb) {
-		if (aabb == null)
-			return;
+    @SubscribeEvent
+    public void renderWorldLast(RenderWorldLastEvent event)
+    {
+        GuiScreen curScreen = Minecraft.getMinecraft().currentScreen;
+        if (curScreen instanceof GuiEditNBTTree)
+        {
+            GuiEditNBTTree screen = (GuiEditNBTTree) curScreen;
+            Entity e = screen.getEntity();
 
-		Entity player = Minecraft.getMinecraft().getRenderViewEntity();
+            if (e != null && e.isEntityAlive())
+                drawBoundingBox(event.context, event.partialTicks, e.getEntityBoundingBox());
+            else if (screen.isTileEntity())
+            {
+                int x = screen.getBlockX();
+                int y = screen.y;
+                int z = screen.z;
+                World world = Minecraft.getMinecraft().theWorld;
+                BlockPos pos = new BlockPos(x, y, z);
+                Block b = world.getBlockState(pos).getBlock();
+                if (b != null)
+                {
+                    b.setBlockBoundsBasedOnState(world, pos);
+                    drawBoundingBox(event.context, event.partialTicks, b.getSelectedBoundingBox(world, pos));
+                }
+            }
+        }
+    }
 
-		double var8 = player.lastTickPosX + (player.posX - player.lastTickPosX) * (double)f;
-		double var10 = player.lastTickPosY + (player.posY - player.lastTickPosY) * (double)f;
-		double var12 = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * (double)f;
+    private void drawBoundingBox(RenderGlobal r, float f, AxisAlignedBB aabb)
+    {
+        if (aabb == null) return;
 
-		aabb = aabb.addCoord(-var8, -var10, -var12);
+        Entity player = Minecraft.getMinecraft().getRenderViewEntity();
 
-		GlStateManager.enableBlend();
-		GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		GlStateManager.color(1.0F, 0.0F, 0.0F, .5F);
-		GL11.glLineWidth(3.5F);
-		GlStateManager.disableTexture2D();
-		GlStateManager.depthMask(false);
+        double var8 = player.lastTickPosX + (player.posX - player.lastTickPosX) * (double) f;
+        double var10 = player.lastTickPosY + (player.posY - player.lastTickPosY) * (double) f;
+        double var12 = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * (double) f;
 
-		PTezzelator tez = PTezzelator.instance;
+        aabb = aabb.addCoord(-var8, -var10, -var12);
 
-		tez.begin(3);
-		tez.vertex(aabb.minX, aabb.minY, aabb.minZ);
-		tez.vertex(aabb.maxX, aabb.minY, aabb.minZ);
-		tez.vertex(aabb.maxX, aabb.minY, aabb.maxZ);
-		tez.vertex(aabb.minX, aabb.minY, aabb.maxZ);
-		tez.vertex(aabb.minX, aabb.minY, aabb.minZ);
-		tez.end();
-		tez.begin(3);
-		tez.vertex(aabb.minX, aabb.maxY, aabb.minZ);
-		tez.vertex(aabb.maxX, aabb.maxY, aabb.minZ);
-		tez.vertex(aabb.maxX, aabb.maxY, aabb.maxZ);
-		tez.vertex(aabb.minX, aabb.maxY, aabb.maxZ);
-		tez.vertex(aabb.minX, aabb.maxY, aabb.minZ);
-		tez.end();
-		tez.begin(1);
-		tez.vertex(aabb.minX, aabb.minY, aabb.minZ);
-		tez.vertex(aabb.minX, aabb.maxY, aabb.minZ);
-		tez.vertex(aabb.maxX, aabb.minY, aabb.minZ);
-		tez.vertex(aabb.maxX, aabb.maxY, aabb.minZ);
-		tez.vertex(aabb.maxX, aabb.minY, aabb.maxZ);
-		tez.vertex(aabb.maxX, aabb.maxY, aabb.maxZ);
-		tez.vertex(aabb.minX, aabb.minY, aabb.maxZ);
-		tez.vertex(aabb.minX, aabb.maxY, aabb.maxZ);
-		tez.end();
+        GlStateManager.enableBlend();
+        GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GlStateManager.color(1.0F, 0.0F, 0.0F, .5F);
+        GL11.glLineWidth(3.5F);
+        GlStateManager.disableTexture2D();
+        GlStateManager.depthMask(false);
 
-		GlStateManager.depthMask(true);
-		GlStateManager.enableTexture2D();
-		GlStateManager.disableBlend();
+        PTezzelator tez = PTezzelator.instance;
 
-	}
+        tez.begin(3);
+        tez.vertex(aabb.minX, aabb.minY, aabb.minZ);
+        tez.vertex(aabb.maxX, aabb.minY, aabb.minZ);
+        tez.vertex(aabb.maxX, aabb.minY, aabb.maxZ);
+        tez.vertex(aabb.minX, aabb.minY, aabb.maxZ);
+        tez.vertex(aabb.minX, aabb.minY, aabb.minZ);
+        tez.end();
+        tez.begin(3);
+        tez.vertex(aabb.minX, aabb.maxY, aabb.minZ);
+        tez.vertex(aabb.maxX, aabb.maxY, aabb.minZ);
+        tez.vertex(aabb.maxX, aabb.maxY, aabb.maxZ);
+        tez.vertex(aabb.minX, aabb.maxY, aabb.maxZ);
+        tez.vertex(aabb.minX, aabb.maxY, aabb.minZ);
+        tez.end();
+        tez.begin(1);
+        tez.vertex(aabb.minX, aabb.minY, aabb.minZ);
+        tez.vertex(aabb.minX, aabb.maxY, aabb.minZ);
+        tez.vertex(aabb.maxX, aabb.minY, aabb.minZ);
+        tez.vertex(aabb.maxX, aabb.maxY, aabb.minZ);
+        tez.vertex(aabb.maxX, aabb.minY, aabb.maxZ);
+        tez.vertex(aabb.maxX, aabb.maxY, aabb.maxZ);
+        tez.vertex(aabb.minX, aabb.minY, aabb.maxZ);
+        tez.vertex(aabb.minX, aabb.maxY, aabb.maxZ);
+        tez.end();
+
+        GlStateManager.depthMask(true);
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+
+    }
+
+    private static class GuiOpener
+    {
+        final Object         target;
+        final NBTTagCompound tag;
+
+        public GuiOpener(Object target, NBTTagCompound tag)
+        {
+            this.tag = tag;
+            this.target = target;
+            MinecraftForge.EVENT_BUS.register(this);
+        }
+
+        @SubscribeEvent
+        public void tick(ClientTickEvent event)
+        {
+            if (target instanceof BlockPos)
+                Minecraft.getMinecraft().displayGuiScreen(new GuiEditNBTTree((BlockPos) target, tag));
+            else if (target instanceof Integer)
+                Minecraft.getMinecraft().displayGuiScreen(new GuiEditNBTTree((int) target, tag));
+            MinecraftForge.EVENT_BUS.unregister(this);
+        }
+
+    }
 }

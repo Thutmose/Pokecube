b7f392dc8289d505aab9a78f9bdf85dcd0713f58
Thutmose <elpatricimo@gmail.com>
2016-02-03 00:04:55 -0400

    Template AI Class for use with nests.

---
+++ Pokecube Core/src/main/java/pokecube/core/ai/thread/aiRunnables/AIReturnHome.java
@@ -0,0 +1,28 @@
+package pokecube.core.ai.thread.aiRunnables;
+
+public class AIReturnHome extends AIBase
+{
+
+    @Override
+    public boolean shouldRun()
+    {
+        //TODO make this run if the pokemob should go home at the end of the "day"
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void run()
+    {
+        // TODO Auto-generated method stub
+        //Should path to gome location.
+    }
+
+    @Override
+    public void reset()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}

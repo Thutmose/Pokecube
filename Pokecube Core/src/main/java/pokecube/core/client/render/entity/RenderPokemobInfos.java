0fced66f45ae947028850c55576f61bec99b06de
Thutmose <elpatricimo@gmail.com>
2016-06-29 22:29:11 -0400

    Tweaks to rendering to better support hardcoded models.

--- Pokecube Core/src/main/java/pokecube/core/client/render/entity/RenderPokemobInfos.java
+++ Pokecube Core/src/main/java/pokecube/core/client/render/entity/RenderPokemobInfos.java
@@ -6,8 +6,11 @@ import org.lwjgl.opengl.GL13;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLiving;
@@ -25,17 +28,16 @@ public abstract class RenderPokemobInfos<T extends EntityLiving> extends RenderL
 {
     public static boolean shouldShow(EntityLivingBase entity)
     {
-        if (((Minecraft) PokecubeCore.getMinecraftInstance()).gameSettings.hideGUI || entity.isBeingRidden())
-        {
-            return false;
-        }
+        if (((Minecraft) PokecubeCore.getMinecraftInstance()).gameSettings.hideGUI
+                || entity.isBeingRidden()) { return false; }
 
         EntityLivingBase player = Minecraft.getMinecraft().thePlayer;
-        if(!entity.addedToChunk || entity.getRidingEntity() == player)return false;
+        if (!entity.addedToChunk || entity.getRidingEntity() == player) return false;
         float d = entity.getDistanceToEntity(player);
         IPokemob pokemob = ((IPokemob) entity);
-        boolean tameFactor = pokemob.getPokemonAIState(IMoveConstants.TAMED) && !pokemob.getPokemonAIState(IMoveConstants.STAYING); 
-        if (( tameFactor && d < 35) || d < 8)
+        boolean tameFactor = pokemob.getPokemonAIState(IMoveConstants.TAMED)
+                && !pokemob.getPokemonAIState(IMoveConstants.STAYING);
+        if ((tameFactor && d < 35) || d < 8)
         {
             return true;
         }
@@ -52,128 +54,138 @@ public abstract class RenderPokemobInfos<T extends EntityLiving> extends RenderL
     }
 
     @Override
-    public void doRender(T entityliving, double d, double d1, double d2, float f, float f1)
+    public void doRender(T entityliving, double d, double d1, double d2, float f, float partialTick)
     {
-        super.doRender(entityliving, d, d1, d2, f, f1);
+        super.doRender(entityliving, d, d1, d2, f, partialTick);
+        renderHp(entityliving, d, d1, d2, f, partialTick);
+    }
 
-        if (shouldShow(entityliving) && d1 != -0.123456)
+    protected void renderHp(T entityliving, double d, double d1, double d2, float f, float partialTick)
+    {
+        if (partialTick <= 1 && RenderPokemobInfos.shouldShow(entityliving))
         {
             float f2 = 1.6F;
             float f3 = 0.01666667F * f2;
             GL11.glPushMatrix();
-            GL11.glTranslatef((float) d + 0.0F, (float) d1 + entityliving.height, (float) d2);
+            EntityLivingBase player = Minecraft.getMinecraft().thePlayer;
+            float dist = entityliving.getDistanceToEntity(player);
+            if (dist < 5) GlStateManager.disableDepth();
+
+            GL11.glTranslatef((float) d + 0.0F, (float) d1 + entityliving.height - 0.35f, (float) d2);
             GL11.glNormal3f(0.0F, 1.0F, 0.0F);
             GL11.glRotatef(-renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
             GL11.glRotatef(renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
             GL11.glScalef(-f3, -f3, f3);
-            GL11.glDisable(GL11.GL_LIGHTING);
-//			GL11.glEnable(GL11.GL_BLEND);
-//			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-            int j = 0xF0F0F0;//61680;
+            int j = 0xF0F0F0;// 61680;
             int k = j % 0x000100;
             int l = j / 0xFFFFFF;
             GL13.glMultiTexCoord2f(GL13.GL_TEXTURE1, k / 1.0F, l / 1.0F);
             PTezzelator tez = PTezzelator.instance;
             int offset = -25;
             double width = 2;
-            GL11.glDisable(GL11.GL_TEXTURE_2D);
-            tez.begin();
+
+            GlStateManager.disableTexture2D();
+            GlStateManager.disableLighting();
+
             int length = 40;
-            float health = entityliving.getHealth();//getHealth()
+            float health = entityliving.getHealth();// getHealth()
 
             if (health < 0)
             {
                 health = 0;
             }
 
-            float maxHealth = entityliving.getMaxHealth();//getMaxHealth()
-            float f7 = health / maxHealth;
-            float f8 = length * 2 * f7;
-            tez.color(0.3F, 0.3F, 0.3F, 1.0F);
-            tez.vertex(-length + f8, offset, 0.0D);
-            tez.vertex(-length + f8, offset + width, 0.0D);
-            tez.vertex(length, offset + width, 0.0D);
-            tez.vertex(length, offset, 0.0D);
-            tez.color(1, 1, 1);
+            // Draw the Health bar
+            float maxHealth = entityliving.getMaxHealth();// getMaxHealth()
+            float relativeHp = health / maxHealth;
+            float shift = length * 2 * relativeHp;
+
+            VertexFormat format;
+            int mode = 7;
+            format = DefaultVertexFormats.POSITION_COLOR;
+
+            tez.begin(mode, format);
+
+            double start = -length + shift;
+            double end = length;
+
+            // Empty Health bar
+            tez.vertex(start, offset, 0.0D).color(0.2F, 0.2F, 0.2F, 1.0F).endVertex();
+            tez.vertex(start, offset + width, 0.0D).color(0.2F, 0.2F, 0.2F, 1.0F).endVertex();
+            tez.vertex(end, offset + width, 0.0D).color(0.2F, 0.2F, 0.2F, 1.0F).endVertex();
+            tez.vertex(end, offset, 0.0D).color(0.2F, 0.2F, 0.2F, 1.0F).endVertex();
+
             int healthColor = 0x00FF00;
 
-            if (f7 > 0.6F)
+            if (relativeHp < 0.6F)
             {
-                tez.color(0.0F, 1.0F, 0.0F, 1.0F);
-            }
-            else
-            {
-                tez.color(1.0F, f7, 0.0F, 1.0F);
-                healthColor = 0xFF0000 + (((int)(0xFF * f7)) * 0x100);
+                healthColor = 0xFF0000 + (((int) (0xFF * relativeHp)) * 0x100);
             }
 
-            tez.vertex(-length, offset, 0.0D);
-            tez.vertex(-length, offset + width, 0.0D);
-            tez.vertex(f8 - length, offset + width, 0.0D);
-            tez.vertex(f8 - length, offset, 0.0D);
-            int exp = ((IPokemob) entityliving).getExp()
-                    - Tools.levelToXp(((IPokemob) entityliving).getExperienceMode(), ((IPokemob) entityliving).getLevel());
-            float maxExp = Tools.levelToXp(((IPokemob) entityliving).getExperienceMode(), ((IPokemob) entityliving).getLevel() + 1)
-                    - Tools.levelToXp(((IPokemob) entityliving).getExperienceMode(), ((IPokemob) entityliving).getLevel());
+            // Coloured Health bar
+            relativeHp *= length * 2;
+            tez.vertex(-length, offset, 0.0D).color(healthColor, 255).endVertex();
+            tez.vertex(-length, offset + width, 0.0D).color(healthColor, 255).endVertex();
+            tez.vertex(relativeHp - length, offset + width, 0.0D).color(healthColor, 255).endVertex();
+            tez.vertex(relativeHp - length, offset, 0.0D).color(healthColor, 255).endVertex();
 
-            if(((IPokemob) entityliving).getLevel() == 100)
-            	maxExp = exp = 1;
-            
-            
+            tez.end();
+
+            int exp = ((IPokemob) entityliving).getExp() - Tools
+                    .levelToXp(((IPokemob) entityliving).getExperienceMode(), ((IPokemob) entityliving).getLevel());
+            float maxExp = Tools.levelToXp(((IPokemob) entityliving).getExperienceMode(),
+                    ((IPokemob) entityliving).getLevel() + 1)
+                    - Tools.levelToXp(((IPokemob) entityliving).getExperienceMode(),
+                            ((IPokemob) entityliving).getLevel());
+
+            if (((IPokemob) entityliving).getLevel() == 100) maxExp = exp = 1;
+
             if (exp < 0 || !((IPokemob) entityliving).getPokemonAIState(IMoveConstants.TAMED))
             {
                 exp = 0;
             }
-            
+            //
             float f62 = maxExp;
             float f72 = exp / f62;
             float f82 = length * 2 * f72;
-            tez.color(0.2F, 0.2F, 0.2F, 1.0F);
-            tez.vertex(-length + f82, offset + width, 0.0D);
-            tez.vertex(-length + f82, offset + width + 1, 0.0D);
-            tez.vertex(length, offset + width + 1, 0.0D);
-            tez.vertex(length, offset + width, 0.0D);
-            tez.color(1, 1, 1);
-            tez.color(0.3F, 0.3F, 0.8F, 1.0F);
-            tez.vertex(-length, offset + width, 0.0D);
-            tez.vertex(-length, offset + width + 1, 0.0D);
-            tez.vertex(f82 - length, offset + width + 1, 0.0D);
-            tez.vertex(f82 - length, offset + width, 0.0D);
+
+            tez.begin(mode, format);
+            // Draw the exp bar
+            tez.vertex(-length + f82, offset + width, 0.0D).color(0.2F, 0.2F, 0.2F, 1.0F).endVertex();
+            tez.vertex(-length + f82, offset + width + 1, 0.0D).color(0.2F, 0.2F, 0.2F, 1.0F).endVertex();
+            tez.vertex(length, offset + width + 1, 0.0D).color(0.2F, 0.2F, 0.2F, 1.0F).endVertex();
+            tez.vertex(length, offset + width, 0.0D).color(0.2F, 0.2F, 0.2F, 1.0F).endVertex();
+            // Fill In exp
+            tez.vertex(-length, offset + width, 0.0D).color(0.3F, 0.3F, 0.8F, 1.0F).endVertex();
+            tez.vertex(-length, offset + width + 1, 0.0D).color(0.3F, 0.3F, 0.8F, 1.0F).endVertex();
+            tez.vertex(f82 - length, offset + width + 1, 0.0D).color(0.3F, 0.3F, 0.8F, 1.0F).endVertex();
+            tez.vertex(f82 - length, offset + width, 0.0D).color(0.3F, 0.3F, 0.8F, 1.0F).endVertex();
             tez.end();
-            GL11.glEnable(GL11.GL_TEXTURE_2D);
+
+            GlStateManager.enableTexture2D();
+
             FontRenderer fontrenderer = getFontRendererFromRenderManager();
 
-            if (((IPokemob) entityliving).getPokemonAIState(IMoveConstants.TAMED) && renderManager.renderViewEntity.equals(((IPokemob) entityliving).getPokemonOwner()))
+            if (((IPokemob) entityliving).getPokemonAIState(IMoveConstants.TAMED))
             {
-                fontrenderer = getFontRendererFromRenderManager();
-                String s = (int)health + "/" + (int)maxHealth;
-                
-                fontrenderer.drawString(s, length - fontrenderer.getStringWidth(s), offset + 5, healthColor);
-         
+                String n;
+                // Your pokemob has white name, other's has gray name.
+                int colour = renderManager.renderViewEntity.equals(((IPokemob) entityliving).getPokemonOwner())
+                        ? 0xFFFFFF : 0xAAAAAA;
+                if ((entityliving.hasCustomName()))
+                {
+                    n = entityliving.getCustomNameTag();
+                }
+                else
+                {
+                    n = ((IPokemob) entityliving).getPokemonDisplayName().getFormattedText();
+                }
+
+                int n1 = length - fontrenderer.getStringWidth(n) / 2;
+
+                n1 = Math.max(0, n1);
+                fontrenderer.drawString(n, -length + n1, offset - 8, colour);
             }
-            
-            //Nickname code
-            
-          if (((IPokemob) entityliving).getPokemonAIState(IMoveConstants.TAMED))// && ))
-          {
-          	String n;// = ((IPokemob) entityliving).getDisplayName();
-          	int colour = renderManager.renderViewEntity.equals(((IPokemob) entityliving).getPokemonOwner())?0xFFFFFF:0xAAAAAA;
-          	int dx = 00;
-          	if((entityliving.hasCustomName())){
-          		n = entityliving.getCustomNameTag();
-          		fontrenderer.drawString(n, length - dx - fontrenderer.getStringWidth(n), offset-8, colour);
-          		}else{
-          			
-          			n = ((IPokemob) entityliving).getPokemonDisplayName().getFormattedText();
-          			
-          			if(n.length() == 8)
-          			 fontrenderer.drawString(n, length - (int)( fontrenderer.getStringWidth(n)*1.45), offset-8, colour);
-          			if(n.length() >= 9)
-          				fontrenderer.drawString(n, length - (int)( fontrenderer.getStringWidth(n)*1.2), offset-8, colour);
-          			if (n.length() < 8)
-          				fontrenderer.drawString(n, length - (int)(fontrenderer.getStringWidth(n)*1.8), offset-8, colour);
-          		}
-          	}
 
             int color = 0xBBBBBB;
 
@@ -188,13 +200,12 @@ public abstract class RenderPokemobInfos<T extends EntityLiving> extends RenderL
 
             String s = "L." + ((IPokemob) entityliving).getLevel();
             fontrenderer.drawString(s, -length, offset + 5, color);
-            GL11.glEnable(GL11.GL_LIGHTING);
-            GL11.glDisable(GL11.GL_BLEND);
-            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+            GlStateManager.enableDepth();
+            GlStateManager.enableLighting();
             GL11.glPopMatrix();
         }
     }
-    
+
     @Override
     public ModelBase getMainModel()
     {
@@ -203,6 +214,6 @@ public abstract class RenderPokemobInfos<T extends EntityLiving> extends RenderL
 
     public void setShadowSize(float size)
     {
-    	this.shadowSize = size;
+        this.shadowSize = size;
     }
 }

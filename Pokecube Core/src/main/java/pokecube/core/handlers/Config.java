c414c53b6f38b2baff52a9d6d99d2a9d25e1f188
Thutmose <elpatricimo@gmail.com>
2016-09-26 23:57:22 -0400

    add configs for location and width of pokemob info messages.

--- Pokecube Core/src/main/java/pokecube/core/handlers/Config.java
+++ Pokecube Core/src/main/java/pokecube/core/handlers/Config.java
@@ -284,6 +284,10 @@ public class Config extends ConfigBase
     @Configure(category = client)
     public int[]               guiOffset                  = { 0, 0 };
     @Configure(category = client)
+    public int[]               messageOffset              = { 0, 0 };
+    @Configure(category = client)
+    public int                 messageWidth               = 150;
+    @Configure(category = client)
     public boolean             guiDown                    = true;
     @Configure(category = client)
     public boolean             autoSelectMoves            = false;

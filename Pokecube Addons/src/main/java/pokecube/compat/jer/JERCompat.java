d72af62782c8d03fad2d3e91740af915661b2c1a
Thutmose <elpatricimo@gmail.com>
2016-11-11 00:52:18 -0400

    Adds fossil generation to JER Compat.

--- Pokecube Addons/src/main/java/pokecube/compat/jer/JERCompat.java
+++ Pokecube Addons/src/main/java/pokecube/compat/jer/JERCompat.java
@@ -10,15 +10,21 @@ import com.google.common.collect.Lists;
 import jeresources.api.IJERAPI;
 import jeresources.api.JERPlugin;
 import jeresources.api.conditionals.LightLevel;
+import jeresources.api.distributions.DistributionBase;
+import jeresources.api.distributions.DistributionSquare;
 import jeresources.api.drop.LootDrop;
 import jeresources.api.drop.PlantDrop;
 import jeresources.api.render.IMobRenderHook;
+import jeresources.api.restrictions.BiomeRestriction;
+import jeresources.api.restrictions.Restriction;
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
+import pokecube.core.PokecubeItems;
 import pokecube.core.database.Database;
 import pokecube.core.database.PokedexEntry;
 import pokecube.core.database.PokedexEntry.SpawnData;
@@ -27,6 +33,7 @@ import pokecube.core.entity.pokemobs.EntityPokemob;
 import pokecube.core.interfaces.IPokemob;
 import pokecube.core.interfaces.PokecubeMod;
 import pokecube.core.items.berries.BerryManager;
+import thut.api.terrain.BiomeDatabase;
 import thut.api.terrain.BiomeType;
 
 public class JERCompat
@@ -91,38 +98,42 @@ public class JERCompat
             List<SpawnBiomeMatcher> matchers = Lists.newArrayList(entry.getSpawnData().matchers.keySet());
             if (matchers.get(0) != null)
             {
-//                SpawnRule spawnRule = matchers.get(0).spawnRule;
-//                float maxLight = 1;
-//                float minLight = 0;
-//                boolean day = true;
-//                boolean night = true;
-//                Float.parseFloat(spawnRule.values.get(SpawnBiomeMatcher.MAXLIGHT));
-//                if (spawnRule.values.containsKey(SpawnBiomeMatcher.DAY))
-//                {
-//                    day = Boolean.parseBoolean(spawnRule.values.get(SpawnBiomeMatcher.DAY));
-//                }
-//                if (spawnRule.values.containsKey(SpawnBiomeMatcher.NIGHT))
-//                {
-//                    night = Boolean.parseBoolean(spawnRule.values.get(SpawnBiomeMatcher.NIGHT));
-//                }
-//                if (spawnRule.values.containsKey(SpawnBiomeMatcher.MINLIGHT))
-//                {
-//                    minLight = Float.parseFloat(spawnRule.values.get(SpawnBiomeMatcher.MINLIGHT));
-//                }
-//                if (spawnRule.values.containsKey(SpawnBiomeMatcher.MAXLIGHT))
-//                {
-//                    maxLight = Float.parseFloat(spawnRule.values.get(SpawnBiomeMatcher.MAXLIGHT));
-//                }
-//                Relative relative = Relative.below;
-//                int light = 15;
-//                if (maxLight != 1)
-//                {
-//                    relative = Relative.below;
-//                    light = (int) (maxLight * 15);
-//                }
-                //TODO see about asking JER to open up this constructor.
-//                LightLevel ret = new LightLevel(light, relative);
-//                return ret;
+                // SpawnRule spawnRule = matchers.get(0).spawnRule;
+                // float maxLight = 1;
+                // float minLight = 0;
+                // boolean day = true;
+                // boolean night = true;
+                // Float.parseFloat(spawnRule.values.get(SpawnBiomeMatcher.MAXLIGHT));
+                // if (spawnRule.values.containsKey(SpawnBiomeMatcher.DAY))
+                // {
+                // day =
+                // Boolean.parseBoolean(spawnRule.values.get(SpawnBiomeMatcher.DAY));
+                // }
+                // if (spawnRule.values.containsKey(SpawnBiomeMatcher.NIGHT))
+                // {
+                // night =
+                // Boolean.parseBoolean(spawnRule.values.get(SpawnBiomeMatcher.NIGHT));
+                // }
+                // if (spawnRule.values.containsKey(SpawnBiomeMatcher.MINLIGHT))
+                // {
+                // minLight =
+                // Float.parseFloat(spawnRule.values.get(SpawnBiomeMatcher.MINLIGHT));
+                // }
+                // if (spawnRule.values.containsKey(SpawnBiomeMatcher.MAXLIGHT))
+                // {
+                // maxLight =
+                // Float.parseFloat(spawnRule.values.get(SpawnBiomeMatcher.MAXLIGHT));
+                // }
+                // Relative relative = Relative.below;
+                // int light = 15;
+                // if (maxLight != 1)
+                // {
+                // relative = Relative.below;
+                // light = (int) (maxLight * 15);
+                // }
+                // TODO see about asking JER to open up this constructor.
+                // LightLevel ret = new LightLevel(light, relative);
+                // return ret;
             }
         }
 
@@ -183,7 +194,25 @@ public class JERCompat
 
     private void registerOres()
     {
+        ItemStack fossilStone = PokecubeItems.getStack("fossilstone");
 
+        DistributionBase distrubution = new DistributionSquare(5, 44, 12 / 265f);
+        Restriction restriction = new Restriction(new BiomeRestriction(BiomeDatabase.getBiome("desertHills"),
+                BiomeDatabase.getBiome("desert"), BiomeDatabase.getBiome("jungle"),
+                BiomeDatabase.getBiome("jungleHills"), BiomeDatabase.getBiome("ocean")));
+        ArrayList<LootDrop> drops = new ArrayList<LootDrop>();
+        LootDrop drop = null;
+        int num = PokecubeItems.fossils.size();
+        for (ItemStack stack : PokecubeItems.fossils.keySet())
+        {
+            if (stack == null) continue;
+            float chance = 1f / (2 * num);
+            drops.add(drop = new LootDrop(stack, chance));
+            drop.minDrop = 1;
+            drop.maxDrop = 1;
+        }
+        drops.add(new LootDrop(new ItemStack(Items.BONE), 0.5f));
+        JERAPI.getWorldGenRegistry().register(fossilStone, distrubution, restriction, drops.toArray(new LootDrop[0]));
     }
 
     private void registerPlants()

013ccf8d915fb8be299f2507bfc452a5bc9cb85e
Thutmose <elpatricimo@gmail.com>
2016-11-15 17:28:39 -0400

    remove some unneeded variables from xml entry loader.

--- Pokecube Core/src/main/java/pokecube/core/database/PokedexEntryLoader.java
+++ Pokecube Core/src/main/java/pokecube/core/database/PokedexEntryLoader.java
@@ -197,20 +197,10 @@ public class PokedexEntryLoader
         @XmlElement(name = "SPECIALEGGSPECIESRULES")
         String          specialEggRules;
         // Drops and items
-        @XmlElement(name = "FOODDROP")
-        String          foodDrop;
-        @XmlElement(name = "COMMONDROP")
-        String          commonDrop;
-        @XmlElement(name = "RAREDROP")
-        String          rareDrop;
-
         @XmlElement(name = "Drop")
         List<Drop>      drops          = Lists.newArrayList();
         @XmlElement(name = "Held")
         List<Drop>      held           = Lists.newArrayList();
-
-        @XmlElement(name = "HELDITEM")
-        String          heldItems;
         // Spawn Rules
         @XmlElement(name = "Spawn")
         List<SpawnRule> spawnRules     = Lists.newArrayList();

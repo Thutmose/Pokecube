e9595d5da850401a9dedee5160f1b86a81f73e77
Thutmose <elpatricimo@gmail.com>
2016-11-18 16:14:03 -0400

    add SM data to database, note that 9 are missing evs atm.

--- Pokecube Core/src/main/java/pokecube/core/database/PokedexEntryLoader.java
+++ Pokecube Core/src/main/java/pokecube/core/database/PokedexEntryLoader.java
@@ -144,7 +144,7 @@ public class PokedexEntryLoader
         public static class LvlUp
         {
             @XmlAnyAttribute
-            Map<QName, String> values;
+            Map<QName, String> values = Maps.newHashMap();
         }
 
         @XmlRootElement(name = "MISC")
@@ -173,7 +173,7 @@ public class PokedexEntryLoader
         public static class Stats
         {
             @XmlAnyAttribute
-            Map<QName, String> values;
+            Map<QName, String> values = Maps.newHashMap();
         }
 
         @XmlAttribute
@@ -1145,12 +1145,40 @@ public class PokedexEntryLoader
                 entry.isStarter = xmlEntry.starter;
                 entry.legendary = xmlEntry.legend;
                 entry.hasShiny = xmlEntry.hasShiny;
-                postIniStats(entry, stats);
-                parseSpawns(entry, stats);
-                parseEvols(entry, stats, true);
+                try
+                {
+                    postIniStats(entry, stats);
+                }
+                catch (Exception e)
+                {
+                    PokecubeMod.log("Error with stats for " + entry);
+                }
+                try
+                {
+                    parseSpawns(entry, stats);
+                }
+                catch (Exception e)
+                {
+                    PokecubeMod.log("Error with spawns for " + entry);
+                }
+                try
+                {
+                    parseEvols(entry, stats, true);
+                }
+                catch (Exception e)
+                {
+                    PokecubeMod.log("Error with evols for " + entry);
+                }
                 if (xmlEntry.special != null)
                 {
-                    parseSpecial(xmlEntry.special, entry);
+                    try
+                    {
+                        parseSpecial(xmlEntry.special, entry);
+                    }
+                    catch (Exception e)
+                    {
+                        PokecubeMod.log("Error with special for " + entry);
+                    }
                 }
             }
         }

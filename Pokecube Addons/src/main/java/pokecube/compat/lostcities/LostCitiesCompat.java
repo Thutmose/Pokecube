1672db3ef6764f52276b1bf14d3cb733912e8ae5
Thutmose <elpatricimo@gmail.com>
2017-06-24 19:12:44 -0400

    Initial phase of Lost Cities Support.
    Automatically assigns subbiomes to the various lost cities structures.

---
+++ Pokecube Addons/src/main/java/pokecube/compat/lostcities/LostCitiesCompat.java
@@ -0,0 +1,26 @@
+package pokecube.compat.lostcities;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Optional;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import pokecube.core.events.onload.InitDatabase;
+import thut.api.terrain.TerrainSegment;
+import thut.lib.CompatClass;
+import thut.lib.CompatClass.Phase;
+
+public class LostCitiesCompat
+{
+
+    @Optional.Method(modid = "lostcities")
+    @CompatClass(takesEvent = false, phase = Phase.CONSTRUCT)
+    public static void initCompat()
+    {
+        MinecraftForge.EVENT_BUS.register(new LostCitiesCompat());
+    }
+
+    @SubscribeEvent
+    public void init(InitDatabase.Post event)
+    {
+        TerrainSegment.defaultChecker = new LostCityTerrainChecker(TerrainSegment.defaultChecker);
+    }
+}
---
+++ Pokecube Addons/src/main/java/pokecube/compat/lostcities/LostCityTerrainChecker.java
@@ -0,0 +1,59 @@
+package pokecube.compat.lostcities;
+
+import mcjty.lostcities.api.ILostChunkGenerator;
+import mcjty.lostcities.api.ILostChunkInfo;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.IChunkGenerator;
+import net.minecraft.world.gen.ChunkProviderServer;
+import pokecube.core.world.terrain.PokecubeTerrainChecker;
+import thut.api.maths.Vector3;
+import thut.api.terrain.BiomeType;
+import thut.api.terrain.TerrainSegment;
+import thut.api.terrain.TerrainSegment.ISubBiomeChecker;
+
+public class LostCityTerrainChecker extends PokecubeTerrainChecker
+{
+    ISubBiomeChecker parent;
+
+    public LostCityTerrainChecker(ISubBiomeChecker parent)
+    {
+        this.parent = parent;
+    }
+
+    @Override
+    public int getSubBiome(World world, Vector3 v, TerrainSegment segment, Chunk chunk, boolean caveAdjusted)
+    {
+        check:
+        if (caveAdjusted)
+        {
+            if (world.getChunkProvider() instanceof ChunkProviderServer)
+            {
+                IChunkGenerator generator = ((ChunkProviderServer) world.getChunkProvider()).chunkGenerator;
+                if (generator instanceof ILostChunkGenerator)
+                {
+                    BlockPos pos = v.getPos();
+                    ILostChunkGenerator lostGenerator = (ILostChunkGenerator) generator;
+                    ILostChunkInfo info = lostGenerator.getChunkInfo(chunk.xPosition, chunk.zPosition);
+                    String type = info.getBuildingType();
+                    if (!info.isCity()) break check;
+                    int streetLevel = lostGenerator.getRealHeight(0);
+                    int maxLevel = lostGenerator.getRealHeight(info.getNumFloors());
+                    int minLevel = lostGenerator.getRealHeight(-info.getNumCellars());
+                    int diff = pos.getY() - streetLevel;
+                    //Give a leeway of 5 blocks above for roof structures..
+                    boolean inStructure = pos.getY() >= minLevel && pos.getY() <= maxLevel + 5;
+                    if (!inStructure) type = null;
+                    // We only want streets to cover close to the ground, above
+                    // that can be sky, below that can be cave.
+                    else if (type == null && diff < 8 && diff > 0) type = "street";
+                    if (type == null) break check;
+                    return BiomeType.getBiome(type, true).getType();
+                }
+            }
+        }
+        return super.getSubBiome(world, v, segment, chunk, caveAdjusted);
+    }
+
+}

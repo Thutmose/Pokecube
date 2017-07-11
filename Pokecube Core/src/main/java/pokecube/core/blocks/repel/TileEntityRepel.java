a9b4edb2602996907c4f62c834e57204ab98161c
Thutmose <elpatricimo@gmail.com>
2017-07-11 16:01:51 -0400

    sync repel radius to clients, since revival shows it now.

--- Pokecube Core/src/main/java/pokecube/core/blocks/repel/TileEntityRepel.java
+++ Pokecube Core/src/main/java/pokecube/core/blocks/repel/TileEntityRepel.java
@@ -1,35 +1,35 @@
 package pokecube.core.blocks.repel;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
 import pokecube.core.events.handlers.SpawnHandler;
 
-/**
- *
- * @author Manchou
- *
- */
+/** @author Manchou */
 public class TileEntityRepel extends TileEntity implements ITickable
 {
-	public byte distance = 10;
+    public byte distance = 10;
     boolean     enabled  = true;
-	
-    public TileEntityRepel() { }
-    
-    public boolean addForbiddenSpawningCoord(){
-    	return SpawnHandler.addForbiddenSpawningCoord(pos, world.provider.getDimension(), distance);
-    }
-    
-    @Override
-    public void invalidate() {
-    	super.invalidate();
-    	removeForbiddenSpawningCoord();
+
+    public TileEntityRepel()
+    {
     }
 
-    /**
-     * Reads a tile entity from NBT.
-     */
+    public boolean addForbiddenSpawningCoord()
+    {
+        return SpawnHandler.addForbiddenSpawningCoord(pos, world.provider.getDimension(), distance);
+    }
+
+    @Override
+    public void invalidate()
+    {
+        super.invalidate();
+        removeForbiddenSpawningCoord();
+    }
+
+    /** Reads a tile entity from NBT. */
     @Override
     public void readFromNBT(NBTTagCompound nbt)
     {
@@ -38,10 +38,11 @@ public class TileEntityRepel extends TileEntity implements ITickable
         enabled = nbt.getBoolean("enabled");
     }
 
-    public boolean removeForbiddenSpawningCoord(){
-    	return SpawnHandler.removeForbiddenSpawningCoord(pos, world.provider.getDimension());
+    public boolean removeForbiddenSpawningCoord()
+    {
+        return SpawnHandler.removeForbiddenSpawningCoord(pos, world.provider.getDimension());
     }
-    
+
     @Override
     public void update()
     {
@@ -58,18 +59,18 @@ public class TileEntityRepel extends TileEntity implements ITickable
             addForbiddenSpawningCoord();
         }
     }
-    
+
     @Override
-    public void validate() {
-    	super.validate();
-    	addForbiddenSpawningCoord();
-    	enabled = true;
+    public void validate()
+    {
+        super.validate();
+        addForbiddenSpawningCoord();
+        enabled = true;
     }
 
-    /**
-     * Writes a tile entity to NBT.
-     * @return 
-     */
+    /** Writes a tile entity to NBT.
+     * 
+     * @return */
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound nbt)
     {
@@ -77,5 +78,41 @@ public class TileEntityRepel extends TileEntity implements ITickable
         nbt.setByte("distance", distance);
         nbt.setBoolean("enabled", enabled);
         return nbt;
+    }
+
+    /** Overriden in a sign to provide the text. */
+    @Override
+    public SPacketUpdateTileEntity getUpdatePacket()
+    {
+        NBTTagCompound nbttagcompound = new NBTTagCompound();
+        if (world.isRemote) return new SPacketUpdateTileEntity(this.getPos(), 3, nbttagcompound);
+        this.writeToNBT(nbttagcompound);
+        return new SPacketUpdateTileEntity(this.getPos(), 3, nbttagcompound);
+    }
+
+    @Override
+    public NBTTagCompound getUpdateTag()
+    {
+        NBTTagCompound nbt = new NBTTagCompound();
+        return writeToNBT(nbt);
+    }
+
+    /** Called when you receive a TileEntityData packet for the location this
+     * TileEntity is currently in. On the client, the NetworkManager will always
+     * be the remote server. On the server, it will be whomever is responsible
+     * for sending the packet.
+     *
+     * @param net
+     *            The NetworkManager the packet originated from
+     * @param pkt
+     *            The data packet */
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt)
+    {
+        if (world.isRemote)
+        {
+            NBTTagCompound nbt = pkt.getNbtCompound();
+            readFromNBT(nbt);
+        }
     }
 }

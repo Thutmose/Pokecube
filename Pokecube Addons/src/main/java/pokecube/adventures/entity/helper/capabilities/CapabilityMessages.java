dc575056d33b28e23afe01d61ac6478043e86247
Thutmose <elpatricimo@gmail.com>
2017-07-19 04:15:34 -0400

    Add in the capability classes from last commit.

---
+++ Pokecube Addons/src/main/java/pokecube/adventures/entity/helper/capabilities/CapabilityMessages.java
@@ -0,0 +1,160 @@
+package pokecube.adventures.entity.helper.capabilities;
+
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import pokecube.adventures.entity.helper.Action;
+import pokecube.adventures.entity.helper.MessageState;
+
+public class CapabilityMessages
+{
+    @CapabilityInject(IHasMessages.class)
+    public static final Capability<IHasMessages> MESSAGES_CAP = null;
+    public static Storage                        storage;
+
+    public static interface IHasMessages
+    {
+        void sendMessage(MessageState state, Entity target, Object... args);
+
+        void doAction(MessageState state, EntityLivingBase target);
+
+        void setMessage(MessageState state, String message);
+
+        void setAction(MessageState state, Action action);
+
+        String getMessage(MessageState state);
+
+        Action getAction(MessageState state);
+    }
+
+    public static class Storage implements Capability.IStorage<IHasMessages>
+    {
+
+        @Override
+        public NBTBase writeNBT(Capability<IHasMessages> capability, IHasMessages instance, EnumFacing side)
+        {
+            NBTTagCompound nbt = new NBTTagCompound();
+            NBTTagCompound messTag = new NBTTagCompound();
+            NBTTagCompound actionTag = new NBTTagCompound();
+            for (MessageState state : MessageState.values())
+            {
+                String message = instance.getMessage(state);
+                if (message != null) messTag.setString(state.name(), message);
+                Action action = instance.getAction(state);
+                if (action != null) actionTag.setString(state.name(), action.getCommand());
+            }
+            nbt.setTag("messages", messTag);
+            nbt.setTag("actions", actionTag);
+            return nbt;
+        }
+
+        @Override
+        public void readNBT(Capability<IHasMessages> capability, IHasMessages instance, EnumFacing side, NBTBase base)
+        {
+            if (!(base instanceof NBTTagCompound)) return;
+            NBTTagCompound nbt = (NBTTagCompound) base;
+            if (!nbt.hasKey("messages")) return;
+            NBTTagCompound messTag = nbt.getCompoundTag("messages");
+            for (MessageState state : MessageState.values())
+            {
+                if (messTag.hasKey(state.name())) instance.setMessage(state, messTag.getString(state.name()));
+            }
+            NBTTagCompound actionTag = nbt.getCompoundTag("actions");
+            for (MessageState state : MessageState.values())
+            {
+                if (actionTag.hasKey(state.name()))
+                    instance.setAction(state, new Action(actionTag.getString(state.name())));
+            }
+        }
+    }
+
+    public static class DefaultMessager implements IHasMessages, ICapabilitySerializable<NBTTagCompound>
+    {
+        Map<MessageState, String> messages = Maps.newHashMap();
+        Map<MessageState, Action> actions  = Maps.newHashMap();
+
+        public DefaultMessager()
+        {
+            messages.put(MessageState.AGRESS, "pokecube.trainer.agress");
+            messages.put(MessageState.ABOUTSEND, "pokecube.trainer.next");
+            messages.put(MessageState.SENDOUT, "pokecube.trainer.toss");
+            messages.put(MessageState.DEFEAT, "pokecube.trainer.defeat");
+            messages.put(MessageState.DEAGRESS, "pokecube.trainer.forget");
+            messages.put(MessageState.GIVEITEM, "pokecube.trainer.drop");
+        }
+
+        @Override
+        public void sendMessage(MessageState state, Entity target, Object... args)
+        {
+            target.addChatMessage(new TextComponentTranslation(messages.get(state), args));
+        }
+
+        @Override
+        public void doAction(MessageState state, EntityLivingBase target)
+        {
+            Action action = actions.get(state);
+            if (action != null && target instanceof EntityPlayer) action.doAction((EntityPlayer) target);
+        }
+
+        @Override
+        public void setMessage(MessageState state, String message)
+        {
+            messages.put(state, message);
+        }
+
+        @Override
+        public void setAction(MessageState state, Action action)
+        {
+            actions.put(state, action);
+        }
+
+        @Override
+        public String getMessage(MessageState state)
+        {
+            return messages.get(state);
+        }
+
+        @Override
+        public Action getAction(MessageState state)
+        {
+            return actions.get(state);
+        }
+
+        @Override
+        public boolean hasCapability(Capability<?> capability, EnumFacing facing)
+        {
+            return capability == MESSAGES_CAP;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T> T getCapability(Capability<T> capability, EnumFacing facing)
+        {
+            return hasCapability(capability, facing) ? (T) this : null;
+        }
+
+        @Override
+        public NBTTagCompound serializeNBT()
+        {
+            return (NBTTagCompound) storage.writeNBT(MESSAGES_CAP, this, null);
+        }
+
+        @Override
+        public void deserializeNBT(NBTTagCompound nbt)
+        {
+            storage.readNBT(MESSAGES_CAP, this, null, nbt);
+        }
+
+    }
+}

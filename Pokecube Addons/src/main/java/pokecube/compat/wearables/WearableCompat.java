0125fa4c30a28834427241aa8c7211b8a1cba521
Thutmose <elpatricimo@gmail.com>
2017-08-02 01:16:04 -0400

    update wearables compat for 1.12 stuff.

--- Pokecube Addons/src/main/java/pokecube/compat/wearables/WearableCompat.java
+++ Pokecube Addons/src/main/java/pokecube/compat/wearables/WearableCompat.java
@@ -81,7 +81,7 @@ public class WearableCompat
                 bag1 = new X3dModel(new ResourceLocation(PokecubeAdv.ID, "models/worn/bag.x3d"));
                 bag2 = new X3dModel(new ResourceLocation(PokecubeAdv.ID, "models/worn/bag.x3d"));
             }
-            int brightness = wearer.getBrightnessForRender(partialTicks);
+            int brightness = wearer.getBrightnessForRender();
             ResourceLocation pass1 = BAG_1;
             ResourceLocation pass2 = BAG_2;
             GL11.glPushMatrix();
@@ -110,7 +110,7 @@ public class WearableCompat
                 int damage = stack.getTagCompound().getInteger("dyeColour");
                 ret = EnumDyeColor.byDyeDamage(damage);
             }
-            Color colour = new Color(ret.getMapColor().colorValue + 0xFF000000);
+            Color colour = new Color(ret.getColorValue() + 0xFF000000);
             int[] col = { colour.getRed(), colour.getBlue(), colour.getGreen(), 255, brightness };
             for (IExtendedModelPart part : bag2.getParts().values())
             {

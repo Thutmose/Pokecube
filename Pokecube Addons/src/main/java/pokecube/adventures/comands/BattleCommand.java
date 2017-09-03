a5b49885bb2d3f1c18d140519c2ce12301040b89
Thutmose <elpatricimo@gmail.com>
2017-09-03 13:28:16 -0400

    Adds a command for initiating a battle with nearest trainer.

---
+++ Pokecube Addons/src/main/java/pokecube/adventures/comands/BattleCommand.java
@@ -0,0 +1,69 @@
+package pokecube.adventures.comands;
+
+import java.util.List;
+
+import com.google.common.base.Predicate;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import pokecube.adventures.entity.helper.capabilities.CapabilityHasPokemobs;
+
+public class BattleCommand extends CommandBase
+{
+
+    @Override
+    public String getCommandName()
+    {
+        return "pokebattle";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender sender)
+    {
+        return "/pokebattle <player>";
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
+    {
+        final EntityPlayer player = getPlayer(server, sender, args[0]);
+        // Use 32 blocks as farthest to look for trainer.
+        final int NEAR = 32 * 32;
+
+        List<EntityLiving> trainers = player.getEntityWorld().getEntities(EntityLiving.class,
+                new Predicate<EntityLiving>()
+                {
+                    @Override
+                    public boolean apply(EntityLiving input)
+                    {
+                        return input.hasCapability(CapabilityHasPokemobs.HASPOKEMOBS_CAP, null)
+                                && input.getDistanceSqToEntity(player) < NEAR;
+                    }
+                });
+        EntityLiving target = null;
+        int closest = NEAR;
+        for (EntityLiving e : trainers)
+        {
+            double d;
+            if ((d = e.getDistanceSqToEntity(player)) < closest)
+            {
+                closest = (int) d;
+                target = e;
+            }
+        }
+        if (target != null)
+        {
+            CapabilityHasPokemobs.IHasPokemobs trainer = target.getCapability(CapabilityHasPokemobs.HASPOKEMOBS_CAP,
+                    null);
+            if (trainer.getTarget() == null) trainer.setTarget(player);
+            else throw new CommandException("%s already has a target.", trainer);
+            return;
+        }
+
+    }
+
+}

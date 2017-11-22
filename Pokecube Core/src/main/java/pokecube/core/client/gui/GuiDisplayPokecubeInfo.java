cf77b404d16eacfac3a30aa565718ed203fd1dff
Thutmose <elpatricimo@gmail.com>
2017-11-21 21:14:38 -0500

    cleanup merge

--- Pokecube Core/src/main/java/pokecube/core/client/gui/GuiDisplayPokecubeInfo.java
+++ Pokecube Core/src/main/java/pokecube/core/client/gui/GuiDisplayPokecubeInfo.java
@@ -27,7 +27,6 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;

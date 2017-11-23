13b94f24b9914f6b27205695aec016a3de0c121e
Thutmose <elpatricimo@gmail.com>
2017-11-23 10:43:03 -0500

    remove some unneeded optional interface stuff.

--- Pokecube Addons/src/main/java/pokecube/adventures/blocks/afa/TileEntityCommander.java
+++ Pokecube Addons/src/main/java/pokecube/adventures/blocks/afa/TileEntityCommander.java
@@ -32,8 +32,7 @@ import pokecube.core.interfaces.pokemob.IHasCommands.Command;
 import pokecube.core.interfaces.pokemob.IHasCommands.IMobCommandHandler;
 import thut.api.maths.Vector3;
 
-@Optional.InterfaceList(value = { @Interface(iface = "li.cil.oc.api.network.SidedComponent", modid = "OpenComputers"),
-        @Interface(iface = "li.cil.oc.api.network.SimpleComponent", modid = "OpenComputers") })
+@Optional.Interface(iface = "li.cil.oc.api.network.SimpleComponent", modid = "OpenComputers")
 public class TileEntityCommander extends TileEntityOwnable implements ITickable, SimpleComponent
 {
     protected boolean          addedToNetwork = false;

f2cb4f090a571ff2f205c718a787b31679071890
Thutmose <elpatricimo@gmail.com>
2017-12-08 07:14:16 -0500

    remove some unneeded fields

--- Pokecube Core/src/main/java/pokecube/core/entity/pokemobs/AnimalChest.java
+++ Pokecube Core/src/main/java/pokecube/core/entity/pokemobs/AnimalChest.java
@@ -4,22 +4,17 @@ import net.minecraft.inventory.InventoryBasic;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import net.minecraftforge.items.wrapper.InvWrapper;
 
 public class AnimalChest extends InventoryBasic
 {
-    final InvWrapper wrapper;
-
     public AnimalChest(String inventoryName, int slotCount)
     {
         super(inventoryName, false, slotCount);
-        wrapper = new InvWrapper(this);
     }
 
     @SideOnly(Side.CLIENT)
     public AnimalChest(ITextComponent invTitle, int slotCount)
     {
         super(invTitle, slotCount);
-        wrapper = new InvWrapper(this);
     }
 }

2fa58f165f4d86a3ad41079d577874de84bf32a3
Thutmose <elpatricimo@gmail.com>
2017-12-20 11:33:31 -0500

    adds an inspect button to pokewatch

--- Pokecube Core/src/main/java/pokecube/core/client/gui/watch/progress/GlobalProgress.java
+++ Pokecube Core/src/main/java/pokecube/core/client/gui/watch/progress/GlobalProgress.java
@@ -1,13 +1,20 @@
 package pokecube.core.client.gui.watch.progress;
 
+import java.io.IOException;
+
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.resources.I18n;
+import net.minecraftforge.fml.client.FMLClientHandler;
 import pokecube.core.client.gui.watch.GuiPokeWatch;
+import pokecube.core.client.gui.watch.util.PageButton;
 import pokecube.core.database.stats.CaptureStats;
 import pokecube.core.database.stats.EggStats;
 import pokecube.core.database.stats.KillStats;
+import pokecube.core.network.packets.PacketPokedex;
 
 public class GlobalProgress extends Progress
 {
+    PageButton button;
 
     public GlobalProgress(GuiPokeWatch watch)
     {
@@ -32,6 +39,11 @@ public class GlobalProgress extends Progress
         String killLine = I18n.format("pokewatch.progress.global.killed", killed1, killed0);
         String hatchLine = I18n.format("pokewatch.progress.global.hatched", hatched1, hatched0);
 
+        int x = watch.width / 2;
+        int y = watch.height / 2 - 5;
+        this.watch.getButtons().add(button = new PageButton(watch.getButtons().size(), x - 50, y + 57, 100, 12,
+                I18n.format("pokewatch.progress.inspect"), this));
+
         for (String line : fontRender.listFormattedStringToWidth(captureLine, 120))
         {
             lines.add(line);
@@ -45,6 +57,15 @@ public class GlobalProgress extends Progress
         for (String line : fontRender.listFormattedStringToWidth(hatchLine, 120))
         {
             lines.add(line);
+        }
+    }
+
+    @Override
+    public void actionPerformed(GuiButton button) throws IOException
+    {
+        if (button.id == this.button.id)
+        {
+            PacketPokedex.sendInspectPacket(true, FMLClientHandler.instance().getCurrentLanguage());
         }
     }
 

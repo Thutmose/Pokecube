1889cdad5385894708584bce09b0932e5c183858
Thutmose <elpatricimo@gmail.com>
2018-01-24 17:49:59 -0500

    tweaks to trading table rendering.

--- Pokecube Core/src/main/java/pokecube/core/client/render/blocks/RenderTradingTable.java
+++ Pokecube Core/src/main/java/pokecube/core/client/render/blocks/RenderTradingTable.java
@@ -9,8 +9,6 @@ import static org.lwjgl.opengl.GL11.glTranslatef;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.item.ItemStack;
@@ -72,38 +70,26 @@ public class RenderTradingTable extends TileEntitySpecialRenderer
         default:
             break;
         }
-        if (table.getStackInSlot(0) != null)
+        if (!table.getStackInSlot(0).isEmpty())
         {
             glPushMatrix();
             glTranslatef(-0.375f, 0.2f, 0);
             glTranslatef(0.5F, 0.8f, 0.5F);
             glScalef(0.15f, 0.15f, 0.15f);
-            GlStateManager.enableAlpha();
-            GlStateManager.enableBlend();
             glRotatef(-90, 0, 1, 0);
-            RenderHelper.disableStandardItemLighting();
             if (table.getStackInSlot(0).getItem() instanceof IPokecube) Minecraft.getMinecraft().getItemRenderer()
                     .renderItem(Minecraft.getMinecraft().player, table.getStackInSlot(0), TransformType.NONE);
-            GlStateManager.disableAlpha();
-            GlStateManager.disableBlend();
-            RenderHelper.enableStandardItemLighting();
             glPopMatrix();
         }
-        if (table.getStackInSlot(1) != null)
+        if (!table.getStackInSlot(1).isEmpty())
         {
             glPushMatrix();
             glTranslatef(0.375f, 0.2f, 0);
             glTranslatef(0.5F, 0.8f, 0.5F);
             glScalef(0.15f, 0.15f, 0.15f);
-            GlStateManager.enableAlpha();
-            GlStateManager.enableBlend();
             glRotatef(90, 0, 1, 0);
-            RenderHelper.disableStandardItemLighting();
             if (table.getStackInSlot(1).getItem() instanceof IPokecube) Minecraft.getMinecraft().getItemRenderer()
                     .renderItem(Minecraft.getMinecraft().player, table.getStackInSlot(1), TransformType.NONE);
-            GlStateManager.disableAlpha();
-            GlStateManager.disableBlend();
-            RenderHelper.enableStandardItemLighting();
             glPopMatrix();
         }
 
